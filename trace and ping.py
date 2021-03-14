29c862e24ada63e1f0db46e4f560280e53643d2b
BuildTools <unconfigured@null.spigotmc.org>
2021-03-14 14:58:23 -0400

    modified:   trace and ping.py

--- trace and ping.py
+++ trace and ping.py
@@ -2,26 +2,42 @@ import sys
 import subprocess
 import re
 import pandas as pd
-
-
+import threading
+import datetime
 def tap(url):
-    route = subprocess.run(f"tracert {url}", shell=True, capture_output=True, encoding='utf-8')
-    ips = re.findall("\\[(\\d*\\.\\d*\\.\\d*\\.\\d*)\\]", route.stdout)[1:]
+    route = subprocess.run(f"traceroute {url}", shell=True, capture_output=True, encoding='utf-8')
+    ips = re.findall("\\((\\d*\\.\\d*\\.\\d*\\.\\d*)\\)", route.stdout)[1:]
+    print(ips)
     return ips
 
 
-def pt(ip, df):
-    p = subprocess.call(f"ping {ip} -c 60")
-    received = re.findall("(\\d*) received")
-    stats = re.findall("= (.*) ms").split("/")
-    stats.insert(0, received[0])
-    stats.insert(0, ip)
+def pt(ip, arr):
+    p = subprocess.run(f"ping {ip} -c 60", shell=True, capture_output=True, encoding='utf-8')
+    received = re.findall("(\\d*) received", p.stdout)[0]
+    if received != "0":
+        stats = re.findall("= (.*) ms", p.stdout)[0].split("/")
+        stats.insert(0, received)
+        stats.insert(0, ip)
+    else:
+        stats = [ip, received, 0,0,0,0]
     row = pd.DataFrame([stats], columns=["ip", "received", "min", "avg", "max", "mdev"])
-    df.append(row)
+    arr.append(row)
 
 
 ips = tap(sys.argv[1])
 df = pd.DataFrame()
-for ip in ips:
-    pt(ip, df)
+rows = []
+threads = [threading.Thread(target=pt, args=(ip, rows)) for ip in ips]
+for thread in threads: 
+    thread.start()
+for thread in threads:
+    thread.join()
+for row in rows:
+    df=df.append(row)
+ip_frame = pd.DataFrame([ips])
+ip_frame = ip_frame.transpose()
+print(ip_frame)
+df = ip_frame.merge(df, how="left", left_on=0, right_on="ip")
+df = df.drop(columns=[0])
 print(df)
+df.to_csv(f"{sys.argv[1]}_{datetime.datetime.now()}.csv", index=False)
